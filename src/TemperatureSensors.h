b39b2c1cc9cf090aef310087b1a4f235f2b6fe25
daniloqb <daniloqb@gmail.com>
2018-05-10 17:41:06 -0300

    refactoring, add Thermistor Helper Class

--- src/TemperatureSensors.h
+++ src/TemperatureSensors.h
@@ -6,23 +6,23 @@
 #include <DallasTemperature.h>
 #include <max6675.h>
 #include <SPI.h>
-
+#include <Thermistor.h>
 
 class TemperatureSensor{
 
   protected:
-    float temperature;
-    float tempAlarmHigh;
-    float tempAlarmLow;
+    float m_temperature;
+    float m_tempAlarmHigh;
+    float m_tempAlarmLow;
     void checkAlarm();
 
   public:
-    boolean alarmHigh;
-    boolean alarmLow;
+    boolean m_alarmHigh;
+    boolean m_alarmLow;
     TemperatureSensor(){};
     float getTemperature(char scale);
-    boolean setHighLimitAlarm(float temp);
-    boolean setLowLimitAlarm(float temp);
+    boolean setHighLimitAlarm(float t_temp);
+    boolean setLowLimitAlarm(float t_temp);
     boolean getHighAlarm();
     boolean getLowAlarm();
     void update(){};
@@ -34,58 +34,58 @@ float TemperatureSensor::getTemperature( char scale='C'){
 
   switch (scale){
     case 'C':
-        return temperature;
+        return m_temperature;
       break;
     case 'K':
-        return (temperature + 273);
+        return (m_temperature + 273);
       break;
     case 'F':
-        return ((temperature * 1.8) + 32);    
+        return ((m_temperature * 1.8) + 32);    
       break;
   }
 }
 
 
-boolean TemperatureSensor::setHighLimitAlarm(float temp){
+boolean TemperatureSensor::setHighLimitAlarm(float t_temp){
   
 //  if (temp < LM35_MIN_TEMPERATURE || temp > LM35_MAX_TEMPERATURE){
 //    return false;
 //  }
 
-  tempAlarmHigh = temp;
+  m_tempAlarmHigh = t_temp;
   return true;
   
 }
 
-boolean TemperatureSensor::setLowLimitAlarm(float temp){
+boolean TemperatureSensor::setLowLimitAlarm(float t_temp){
   
   //if (temp < LM35_MIN_TEMPERATURE || temp > LM35_MAX_TEMPERATURE){
   //  return false;
  // }
 
-  tempAlarmLow = temp;
+  m_tempAlarmLow = t_temp;
   return true;
   
 }
 
 boolean TemperatureSensor::getHighAlarm(){
-  return alarmHigh;
+  return m_alarmHigh;
 }
 
 boolean TemperatureSensor::getLowAlarm(){
-  return alarmLow;
+  return m_alarmLow;
 }
 
 void TemperatureSensor::checkAlarm(){
-    if (temperature >= tempAlarmHigh){
-    alarmHigh = 1;
+    if (m_temperature >= m_tempAlarmHigh){
+    m_alarmHigh = 1;
   } else {
-    alarmHigh = 0;
+    m_alarmHigh = 0;
   }
-  if (temperature <= tempAlarmLow){
-    alarmLow = 1;
+  if (m_temperature <= m_tempAlarmLow){
+    m_alarmLow = 1;
   } else {
-    alarmLow = 0;
+    m_alarmLow = 0;
   }
 }
 
@@ -96,72 +96,72 @@ void TemperatureSensor::checkAlarm(){
 class TemperatureSensorLM35 : public TemperatureSensor{
 
   private:
-    int sensor_pin;
-    int resolution;
-    float adc_reference;
-    float voltage_reference;
+    int m_pin;
+    int m_resolution;
+    float m_adc;
+    float m_voltage;
    
     
   public:
 
-void update();
+    void update();
 
-    TemperatureSensorLM35(int pin_, int resolution_);
-    TemperatureSensorLM35(int pin_, int resolution_, float adc_, float voltage_); 
+    TemperatureSensorLM35(int t_pin, int t_resolution);
+    TemperatureSensorLM35(int t_pin, int t_resolution, float t_adc, float t_voltage); 
 
-    void setPin(int pin_);
-    void setResolution(int resolution_);
-    void setAdcReference(float adc_);
-    void setVoltageReference(float voltage_);
+    void setPin(int t_pin);
+    void setResolution(int t_resolution);
+    void setAdcReference(float t_adc);
+    void setVoltageReference(float t_voltage);
   
 };
 
-TemperatureSensorLM35::TemperatureSensorLM35(int pin_, int resolution_=16){
-  sensor_pin = pin_;
-  resolution = resolution_;
-  adc_reference = 1024.0;
-  voltage_reference = 5.0;
+TemperatureSensorLM35::TemperatureSensorLM35(int t_pin, int t_resolution=16){
+  m_pin = t_pin;
+  m_resolution = t_resolution;
+  m_adc = 1024.0;
+  m_voltage = 5.0;
 
-  alarmHigh = 0;
-  alarmLow = 0;
-  tempAlarmHigh = 150;
-  tempAlarmLow = -55;
+  m_alarmHigh = 0;
+  m_alarmLow = 0;
+  m_tempAlarmHigh = 150;
+  m_tempAlarmLow = -55;
 }
 
-TemperatureSensorLM35::TemperatureSensorLM35(int pin_, int resolution_, float adc_, float voltage_){
+TemperatureSensorLM35::TemperatureSensorLM35(int t_pin, int t_resolution, float t_adc, float t_voltage){
 
-  sensor_pin = pin_;
-  resolution = resolution_;
-  adc_reference = adc_;
-  voltage_reference = voltage_;
+  m_pin = t_pin;
+  m_resolution = t_resolution;
+  m_adc = t_adc;
+  m_voltage = t_voltage;
 
-  alarmHigh = 0;
-  alarmLow = 0;
-  tempAlarmHigh = 150;
-  tempAlarmLow = -55;
+  m_alarmHigh = 0;
+  m_alarmLow = 0;
+  m_tempAlarmHigh = 150;
+  m_tempAlarmLow = -55;
 }
 
 void TemperatureSensorLM35::update(){
   float values = 0;
 
 
-  for (int i = 0; i < resolution; i++){
-    values += analogRead(sensor_pin);
+  for (int i = 0; i < m_resolution; i++){
+    values += analogRead(m_pin);
   }
-  values /= resolution;
-  float milivolts = ( values / adc_reference) * (voltage_reference * 1000);
+  values /= m_resolution;
+  float milivolts = ( values / m_adc) * (m_voltage * 1000);
   
-  temperature = milivolts / 10;
+  m_temperature = milivolts / 10;
 
   checkAlarm();
 }
 
-void TemperatureSensorLM35::setAdcReference(float adc_){
-  adc_reference = adc_;
+void TemperatureSensorLM35::setAdcReference(float t_adc){
+  m_adc = t_adc;
 }
 
-void TemperatureSensorLM35::setVoltageReference(float voltage_){
-  voltage_reference = voltage_;
+void TemperatureSensorLM35::setVoltageReference(float t_voltage){
+  m_voltage = t_voltage;
 }
 
 
@@ -170,47 +170,45 @@ void TemperatureSensorLM35::setVoltageReference(float voltage_){
 
 class TemperatureSensorDallas : public TemperatureSensor{
   private:
-    int sensor_index;
-    int onewire_pin;
-    int resolution;
-    OneWire * wo;
-    DallasTemperature * sensors;
+    int m_sensor_index;
+    int m_onewire_pin;
+    int m_resolution;
+    OneWire * m_wo;
+    DallasTemperature * m_sensors;
 
   public:
   TemperatureSensorDallas();
-  TemperatureSensorDallas(int _onewire_pin);
+  TemperatureSensorDallas(int t_onewire_pin);
 
     void begin();
     void update();
-
-
 };
 
 
  TemperatureSensorDallas::TemperatureSensorDallas(){
   
-  onewire_pin = ONEWIRE_PIN_DEFAULT;
-  sensor_index = 0;
-  resolution = DALLAS_DEFAULT_RESOLUTION;
+  m_onewire_pin = ONEWIRE_PIN_DEFAULT;
+  m_sensor_index = 0;
+  m_resolution = DALLAS_DEFAULT_RESOLUTION;
  }
 
-  TemperatureSensorDallas::TemperatureSensorDallas(int _onewire_pin){
+  TemperatureSensorDallas::TemperatureSensorDallas(int t_onewire_pin){
 
-  onewire_pin = _onewire_pin;
-  sensor_index = 0;
-  resolution = DALLAS_DEFAULT_RESOLUTION;
+  m_onewire_pin = t_onewire_pin;
+  m_sensor_index = 0;
+  m_resolution = DALLAS_DEFAULT_RESOLUTION;
  }
 
 void TemperatureSensorDallas::begin(){
 
-  wo = new OneWire(onewire_pin);
-  sensors = new DallasTemperature(wo);
-  sensors->begin();
+  m_wo = new OneWire(m_onewire_pin);
+  m_sensors = new DallasTemperature(m_wo);
+  m_sensors->begin();
 }
 
 void TemperatureSensorDallas::update(){
-  sensors->requestTemperatures();
-  temperature = sensors->getTempCByIndex(sensor_index);
+  m_sensors->requestTemperatures();
+  m_temperature = m_sensors->getTempCByIndex(m_sensor_index);
 
   checkAlarm();
 }
@@ -218,44 +216,81 @@ void TemperatureSensorDallas::update(){
 class TemperatureSensorThermopar : public TemperatureSensor{
 
   private:
-    MAX6675 thermocouple;
+    MAX6675 m_thermocouple;
 
   public:
     TemperatureSensorThermopar();
-    TemperatureSensorThermopar(int8_t SCLK, int8_t CS, int8_t MISO, double OFFSET = 0.0);
-    TemperatureSensorThermopar(int8_t CS, double OFFSET = 0.0);
+    TemperatureSensorThermopar(int8_t SCLK, int8_t CS, int8_t MISO, double OFFSET);
+    TemperatureSensorThermopar(int8_t CS, double OFFSET);
    
-    begin(int8_t SCLK, int8_t CS, int8_t MISO, double OFFSET = 0.0);
-    begin(int8_t CS, double OFFSET = 0.0);
-    update();
+    void begin(int8_t SCLK, int8_t CS, int8_t MISO, double OFFSET);
+    void begin(int8_t CS, double OFFSET);
+    void update();
     
 };
 
 TemperatureSensorThermopar::TemperatureSensorThermopar(){
 
-  thermocouple = MAX6675();
+  m_thermocouple = MAX6675();
 }
 
 TemperatureSensorThermopar::TemperatureSensorThermopar(int8_t SCLK, int8_t CS, int8_t MISO, double OFFSET = 0.0){
 
-  thermocouple = MAX6675(SCLK, CS, MISO, OFFSET);
+  m_thermocouple = MAX6675(SCLK, CS, MISO, OFFSET);
 }
 
 TemperatureSensorThermopar::TemperatureSensorThermopar(int8_t CS, double OFFSET = 0.0){
 
-  thermocouple = MAX6675(CS, OFFSET);
+  m_thermocouple = MAX6675(CS, OFFSET);
 }
 
-TemperatureSensorThermopar::begin(int8_t SCLK, int8_t CS, int8_t MISO, double OFFSET){
-  thermocouple.begin(SCLK, CS, MISO, OFFSET);
+void TemperatureSensorThermopar::begin(int8_t SCLK, int8_t CS, int8_t MISO, double OFFSET=0.0){
+  m_thermocouple.begin(SCLK, CS, MISO, OFFSET);
 }
 
-TemperatureSensorThermopar::begin(int8_t CS, double OFFSET){
-  thermocouple.begin(CS,OFFSET);
+void TemperatureSensorThermopar::begin(int8_t CS, double OFFSET=0.0){
+ m_thermocouple.begin(CS,OFFSET);
 }
 
-TemperatureSensorThermopar::update(){
-  temperature = thermocouple.readCelsius();
+void TemperatureSensorThermopar::update(){
+  m_temperature = m_thermocouple.readCelsius();
+}
+
+class TemperatureSensorNTC : public TemperatureSensor{
+  private:
+    Thermistor * m_sensor;
+ 
+  public:
+  TemperatureSensorNTC();
+  TemperatureSensorNTC(uint8_t t_pin_);
+  TemperatureSensorNTC(uint8_t t_pin_, int t_ro, int t_rf);
+  TemperatureSensorNTC(uint8_t t_pin_, int t_ro, int t_rf, int t_beta);
+
+    void begin();
+    void update();
+};
+
+TemperatureSensorNTC::TemperatureSensorNTC(){
+  m_sensor = new Thermistor();
+}
+
+TemperatureSensorNTC::TemperatureSensorNTC(uint8_t t_pin_){
+  m_sensor = new Thermistor(t_pin_);
+}
+
+TemperatureSensorNTC::TemperatureSensorNTC(uint8_t t_pin_, int t_ro, int t_rf){
+  m_sensor = new Thermistor(t_pin_, t_ro, t_rf);
+}
+
+TemperatureSensorNTC::TemperatureSensorNTC(uint8_t t_pin_, int t_ro, int t_rf, int t_beta){
+  m_sensor = new Thermistor(t_pin_, t_ro, t_rf, t_beta);
+}
+
+void TemperatureSensorNTC::begin(){}
+
+void TemperatureSensorNTC::update(){
+  
+  m_temperature = m_sensor->getTemperature();
 }
 
 
